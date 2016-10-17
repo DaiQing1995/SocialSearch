b959e1fedc8314460114ff590366baec129fcf9e
zhangxueting0152 <923944819@qq.com>
2016-10-17 21:01:14 +0800

    add friendship

--- src/com/cn/dataMine/Friendships_FriendsGet.java
+++ src/com/cn/dataMine/Friendships_FriendsGet.java
@@ -1,11 +1,17 @@
-package weibo4j.examples.friendships;
+package com.cn.dataMine;
+
+import java.util.List;
+
+import com.cn.DataBaseService.Friendships_FriendsInput;
+import com.cn.DataBaseService.UsersDataInput;
 
 import weibo4j.Friendships;
+import weibo4j.Users;
 import weibo4j.model.User;
 import weibo4j.model.UserWapper;
 import weibo4j.model.WeiboException;
 
-public class GetFriendsById {
+public class Friendships_FriendsGet {
 
 	public static void main(String[] args) {
 		String access_token = "2.004DezPC6P3DdD00518038e19iDRoD";
@@ -13,19 +19,12 @@ public class GetFriendsById {
 		Friendships fm = new Friendships(access_token);
 		try {
 			UserWapper users = fm.getFriendsByID(id);
-			int i = 0;
-			System.out.println("heheda " + users.getUsers().size());
-			for(User u : users.getUsers()){
-				System.out.println(u.toString());
-				i++;
+			for (User u : users.getUsers()) {
+				Friendships_FriendsInput.friendshipsFriendsInsert(u);
 			}
-			System.out.println("dq : count = " + i);
-			System.out.println(users.getNextCursor());
-			System.out.println(users.getPreviousCursor());
-			System.out.println(users.getTotalNumber());
 		} catch (WeiboException e) {
 			e.printStackTrace();
 		}
-	}
 
+	}
 }
