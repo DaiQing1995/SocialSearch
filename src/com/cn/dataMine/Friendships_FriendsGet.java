3df8bc580f23a757af15cf55d69965be02af1704
daiqing <dq_199510@163.com>
2016-10-18 16:00:36 +0800

    delete redundant Java Class

--- src/com/cn/dataMine/Friendships_FriendsGet.java
+++ src/com/cn/dataMine/Friendships_FriendsGet.java
@@ -1,26 +1,23 @@
 package com.cn.dataMine;
 
-import java.util.List;
-
-import com.cn.DataBaseService.Friendships_FriendsInput;
 import com.cn.DataBaseService.UsersDataInput;
+import com.cn.util.TokenGet;
 
 import weibo4j.Friendships;
-import weibo4j.Users;
 import weibo4j.model.User;
 import weibo4j.model.UserWapper;
 import weibo4j.model.WeiboException;
-
+//获取用户关注列表
 public class Friendships_FriendsGet {
 
 	public static void main(String[] args) {
-		String access_token = "2.004DezPC6P3DdD00518038e19iDRoD";
-		String id = "2068602681";
+		String access_token = TokenGet.TOKEN;
+		String id = TokenGet.UID;
 		Friendships fm = new Friendships(access_token);
 		try {
 			UserWapper users = fm.getFriendsByID(id);
 			for (User u : users.getUsers()) {
-				Friendships_FriendsInput.friendshipsFriendsInsert(u);
+				UsersDataInput.userDataInsert(u);
 			}
 		} catch (WeiboException e) {
 			e.printStackTrace();
--- src/com/cn/dataMine/UserDataGet.java
+++ src/com/cn/dataMine/UserDataGet.java
@@ -6,7 +6,7 @@ import com.cn.DataBaseService.TargetUsers;
 import com.cn.DataBaseService.UsersDataInput;
 import weibo4j.model.User;
 import weibo4j.model.WeiboException;
-
+//根据UID 和 domain 获取用户
 public class UserDataGet {
 	public static void main(String[] args) throws WeiboException {
 		ArrayList<User> userList = TargetUsers.GetTargetUsers();
